f74fe2baf86edc6b618439dc1f4d0696936cb13c
Matt <grymb3l@gmail.com>
2019-05-26 21:58:45 +0800

    Sqlite Introduction

---
+++ L4rry/L4rry/Items/Reminder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4rry.Items
+{
+    public class Reminder
+    {
+        public String UserID;
+        public String Task;
+        public DateTime Time;
+        public bool IsRepeatable;
+
+        public Reminder(String UserID, String Task, DateTime Time, bool IsRepeatable)
+        {
+            this.UserID = UserID;
+            this.Task = Task;
+            this.Time = Time;
+            this.IsRepeatable = IsRepeatable;
+        }
+    }
+}
--- L4rry/L4rry/Utilities/SQLiteClass.cs
+++ L4rry/L4rry/Utilities/SQLiteClass.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.Data.SqlClient;
 using System.IO;
+using L4rry.Items;
 
 namespace L4rry.Utilities
 {
@@ -30,7 +31,7 @@ namespace L4rry.Utilities
                 m_dbConnection = new SQLiteConnection("Data Source=" + fileLocation + ";Version=3;");
                 m_dbConnection.Open();
 
-                string sql = "CREATE TABLE Images (Image BLOB PRIMARY KEY NOT NULL )";
+                string sql = "CREATE TABLE Reminders (User TEXT, Task TEXT, Time TEXT, Repeat NUMERIC)";
                 SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
                 command.ExecuteNonQuery();
             }
@@ -38,6 +39,49 @@ namespace L4rry.Utilities
             fileLocation = ProgramFilesx86() + "\\L4rry\\Information.dat";
             m_dbConnection = new SQLiteConnection("Data Source=" + fileLocation + ";Version=3;");
             m_dbConnection.Open();
+        }
+
+        public void RemoveReminder(Reminder r)
+        {
+            using (SQLiteCommand cmd = new SQLiteCommand("DELETE FROM Reminders WHERE User=$u AND Task=$t AND Time=$d AND Repeat=$r", m_dbConnection))
+            {
+                cmd.Parameters.AddWithValue("$u", r.UserID);
+                cmd.Parameters.AddWithValue("$t", r.Task);
+                cmd.Parameters.AddWithValue("$d", r.Time);
+                cmd.Parameters.AddWithValue("$r", r.IsRepeatable);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void AddReminder(Reminder r)
+        {
+            using (SQLiteCommand cmd = new SQLiteCommand("INSERT INTO Reminders " +
+                    "(User, Task, Time, Repeat) VALUES ($u,$t,$d,$r)", m_dbConnection))
+            {
+                cmd.Parameters.AddWithValue("$u", r.UserID);
+                cmd.Parameters.AddWithValue("$t", r.Task);
+                cmd.Parameters.AddWithValue("$d", r.Time);
+                cmd.Parameters.AddWithValue("$r", r.IsRepeatable);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Reminder GetLatestReminder()
+        {
+            String sql = "SELECT * FROM Reminders ORDER BY Time asc";
+            using (SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return new Reminder(reader.GetString(0),reader.GetString(1),reader.GetDateTime(2),reader.GetBoolean(3));
+                    }
+                }
+            }
+            return null;
         }
 
         public void CloseCon()
